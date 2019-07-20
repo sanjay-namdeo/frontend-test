26029a22c6f97099bca984e9461eeb5b7f4e1529
Sanjay Namdeo <sanjay.namdeo@live.com>
2019-07-20 19:11:34 +0200

    Added list and search components, giphy service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
+import { GiphyService } from './service/giphy.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
