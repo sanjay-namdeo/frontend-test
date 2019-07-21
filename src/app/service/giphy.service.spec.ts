1b77236aafd6958ad211fb34b425fc1d54b2b273
Sanjay Namdeo <sanjay.namdeo@live.com>
2019-07-21 22:07:53 +0200

    Added test to check media url

--- src/app/service/giphy.service.spec.ts
+++ src/app/service/giphy.service.spec.ts
@@ -2,14 +2,26 @@ import { TestBed } from '@angular/core/testing';
 
 import { GiphyService } from './giphy.service';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
 
 describe('GiphyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
-  }));
+  let service: GiphyService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule]
+    });
+    service = TestBed.get(GiphyService);
+  });
 
   it('should be created', () => {
-    const service: GiphyService = TestBed.get(GiphyService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return giphy media image url', () => {
+    const url = service.getFullUrl('testId');
+    expect(url).toEqual(`https://media.giphy.com/media/testId/giphy.gif`);
   });
 });
